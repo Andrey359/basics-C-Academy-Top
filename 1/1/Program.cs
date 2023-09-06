2e8fefe58f8b8f729145dd923dba53873eafbb36
ТОП <>
2023-09-06 21:08:20 +0300

    Добавьте файлы проекта.

---
+++ 1/1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+class Product
+{
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private int Price { get; set; }
+    private string Category { get; set; }
+    private string? Description { get; set; }
+
+    public Product(int id, string name, int price, string category, string description)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+        Category = category;
+        Description = description ;
+    }
+
+    static int NextID = 1;
+    static public Product create()
+    {
+        Console.Write("Имя продукта ");
+        string name = Console.ReadLine();
+        Console.Write("Цена продукта ");
+        int price = int.Parse(Console.ReadLine());
+        Console.Write("Категория продукта ");
+        string category = Console.ReadLine();
+        Console.Write("Описание продукта ");
+        string description = Console.ReadLine();
+        if (description.Length == 0)
+        {
+            description = null;
+        }
+        return new Product(NextID++, name, price, category, description);
+    }
+
+    public override string ToString()
+    {
+        return "Product(" + Id + ", " + Name + ", " + Price + ", " + Category + ", " + (Description ?? "no_description") + ")";
+    }
+
+
+    public static Product CreateRandom()
+    {
+        return new Product(
+            NextID++, 
+            Util.RandomString(10),
+            new Random().Next() % 1000,
+            Util.RandomString(10),
+            null);
+    }
+}
+
+class Util
+{
+
+    public static string RandomString(int length)
+    {
+        Random random = new Random();
+        string text = "";
+        for (int i = 0; i < length; i++)
+        {
+            text += (char)('a' + (random.Next() % 26));
+        }
+        return text;
+    }
+}
+
+class MainClass
+{
+
+    public static void Main() 
+    {
+        Product product = Product.CreateRandom();
+        Console.WriteLine(product);
+        Console.WriteLine (Util.RandomString(1));
+    }
+    
+}
---
+++ Дата/Дата/Program.cs
@@ -0,0 +1,63 @@
+﻿public class MainClass
+{
+     static string checkYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return "високосный год";
+            }
+
+            else if (year % 100 == 0)
+            {
+                return "не високосный год";
+            }
+
+            else if (year % 4 == 0)
+            {
+                return "високосный год";
+            }
+
+            else
+            {
+                return "не високосный год";
+            }
+        }
+        static string checkMonth(int month)
+        {
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "зима";
+                case 3:
+                case 4:
+                case 5:
+                    return "весна";
+                case 6:
+                case 7:
+                case 8:
+                    return "лето";
+                case 9:
+                case 10:
+                case 11:
+                    return "осень";
+            }
+            return "Ошибка, нет такого месяца";
+        }
+    public static void Main()
+    {
+        string dateText;
+        int year, month, day;
+
+        dateText = Console.ReadLine();
+        string[] date = dateText.Split(".");
+
+        year = int.Parse(date[0]);
+        month = int.Parse(date[1]);
+        day = int.Parse(date[2]);
+
+        Console.WriteLine(checkMonth(month) + ", " + checkYear(year));
+
+    }
+}
